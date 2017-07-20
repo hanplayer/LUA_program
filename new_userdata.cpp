046ed2eb54660651e339431b62d3ac0a38ab7119
hanplayer <hanfengzhe1@163.com>
2017-07-20 11:29:18 +0800

    模拟lua脚本

--- new_userdata.cpp
+++ new_userdata.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <lua.hpp>
+extern "C"
+{
 #include <lauxlib.h>
 #include <lualib.h>
+#include <lua.h>
+}
 #include <string>
 #include <iostream>
-
-struct Student
+ struct Student
 {
 public:
 	std::string name;
@@ -19,7 +21,7 @@ public:
 //
 static int NewStudents(lua_State *L)
 {
-	int n = luaL_checkint(L,1);
+	int n = luaL_checkinteger(L,1);
 	int size = n * sizeof(Student);
 	struct Student *p = (struct Student *)lua_newuserdata(L,size);
 	return 1;
@@ -29,30 +31,38 @@ static int SetName(lua_State *L)
 {
 	struct Student * p_st = (struct Student*)lua_touserdata(L,1);
 	std::string name = luaL_checkstring(L,2);
-	int id = luaL_checkint(L,3);
+	int id = luaL_checkinteger(L,3);
 	(p_st + id - 1)->name = name;
+	std::cout<<name<<std::endl;
 	return 0;
 }
 static int GetName(lua_State *L)
 {
         struct Student * p_st = (struct Student*)lua_touserdata(L,1);
         std::string name;
-        int id = luaL_checkint(L,2);
+        int id = luaL_checkinteger(L,2);
 	name = (p_st + id - 1)->name;
-	lua_pushstring(L,name);
+	std::cout<<name<<std::endl;
+	lua_pushstring(L,name.c_str());
 	return 1;
 }
 
-const char * test = "tmp = new(2); tmp."
+const char * test = "tmp = new(2); setname(tmp,2\"xiaoming\"); print(getname(tmp,2));";
 int main()
 {
-	lua_State * L;
-	luaL_openlib(L);
+	static lua_State * L;
+	L = luaL_newstate();
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
+	luaL_openlibs(L);
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
 	lua_register(L,"new",NewStudents);
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
 	lua_register(L,"setname",SetName);
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
 	lua_register(L,"getname",GetName);
-	
-	
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
+	luaL_dostring(L,test);
+	std::cout<<"LINE:"<<__LINE__<<std::endl;
 	lua_close(L);
 	return 0;
 }
